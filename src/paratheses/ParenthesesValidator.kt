296844e739481b7c1feb2d38846b1f695bdd1af1
t-andryukha <t-andryukha@yandex.ru>
2018-06-29 15:04:25 +0300

    parentheses validator tests

---
+++ src/paratheses/ParenthesesValidator.kt
@@ -0,0 +1,8 @@
+package paratheses
+
+class ParenthesesValidator {
+    fun isBalanced(parentheses: String): Boolean {
+        TODO()
+    }
+
+}
---
+++ test/paratheses/ParenthesesValidatorTest.kt
@@ -0,0 +1,50 @@
+package paratheses
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+/**
+ * Write a program to determine if the the parentheses (),
+the brackets [], and the braces {}, in a string are balanced.
+
+For example:
+
+{{)(}} is not balanced because ) comes before (
+
+({)} is not balanced because ) is not balanced between {}
+and similarly the { is not balanced between ()
+
+[({})] is balanced
+
+{}([]) is balanced
+
+{()}[[{}]] is balanced
+ */
+internal class ParenthesesValidatorTest {
+
+    @Test
+    internal fun `should be not balanced when ) comes before (`() {
+        assertFalse(ParenthesesValidator().isBalanced("{{)(}}"))
+    }
+
+    @Test
+    internal fun `should be not balanced when ) is not balanced between {}`() {
+        assertFalse(ParenthesesValidator().isBalanced("({)}"))
+    }
+
+    @Test
+    internal fun `should be balanced #1`() {
+        assertFalse(ParenthesesValidator().isBalanced("[({})]"))
+    }
+
+    @Test
+    internal fun `should be balanced #2`() {
+        assertFalse(ParenthesesValidator().isBalanced("{}([])"))
+    }
+
+    @Test
+    internal fun `should be balanced #3`() {
+        assertFalse(ParenthesesValidator().isBalanced("{()}[[{}]]"))
+    }
+
+}
