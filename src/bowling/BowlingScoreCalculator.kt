12d198dab3e37b5e0b2f766b599e1c1fa754496a
t-andryukha <t-andryukha@yandex.ru>
2018-07-02 14:51:05 +0300

    bowling score calculator tests

---
+++ src/bowling/BowlingScoreCalculator.kt
@@ -0,0 +1,7 @@
+package bowling
+
+class BowlingScoreCalculator {
+    fun getScore(gameResult: String): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
