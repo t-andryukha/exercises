03874df7964cd97606202d1797412474e8a32e59
andrey.taran <andrey.taran@cengage.com>
2018-07-24 16:23:55 +0300

    tests for GCDFinder

---
+++ src/main/kotlin/com/exercise/gcd/GCDFinder.kt
@@ -0,0 +1,8 @@
+package com.exercise.gcd
+
+class GCDFinder {
+    fun find(num1: Int, num2: Int): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
--- src/test/kotlin/com/exercise/gcd/GCDFinderTest.kt
+++ src/test/kotlin/com/exercise/gcd/GCDFinderTest.kt
@@ -1,9 +1,25 @@
+import com.exercise.gcd.GCDFinder
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 internal class GCDFinderTest{
     @Test
-    internal fun `should TODO`() {
-        assertEquals(300, 300)
+    internal fun `should return 3 given 3 and 6`() {
+        assertEquals(3, GCDFinder().find(3,6))
+    }
+    
+    @Test
+    internal fun `should return 12 given 12 and 24`() {
+        assertEquals(12, GCDFinder().find(12,24))
+    }
+
+    @Test
+    internal fun `should return 1 given 13 and 25`() {
+        assertEquals(1, GCDFinder().find(13,25))
+    }
+
+    @Test
+    internal fun `should return 40 given 3400 and 360`() {
+        assertEquals(40, GCDFinder().find(3400,360))
     }
 }
