04debea397096c131c4fdcbfb56dd0a38080d216
t-andryukha <t-andryukha@yandex.ru>
2018-07-25 14:51:37 +0300

    optimization

--- src/main/kotlin/com/exercise/gcd/GCDFinder.kt
+++ src/main/kotlin/com/exercise/gcd/GCDFinder.kt
@@ -2,7 +2,7 @@ package com.exercise.gcd
 
 class GCDFinder {
     fun find(num1: Int, num2: Int): Int {
-        for (divisor in Math.min(num1, num2) downTo 1) {
+        for (divisor in Math.min(num1, num2) downTo 2) {
             if (num1 % divisor == 0 && num2 % divisor == 0) return divisor
         }
         return 1
