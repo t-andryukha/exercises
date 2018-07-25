b04100b4b12d68abf95ae25910cab459d0409d09
t-andryukha <t-andryukha@yandex.ru>
2018-07-25 16:37:35 +0300

    iterative optimization

--- src/main/kotlin/com/exercise/gcd/GCDFinder.kt
+++ src/main/kotlin/com/exercise/gcd/GCDFinder.kt
@@ -1,14 +1,26 @@
 package com.exercise.gcd
 
+/**
+ * Find Greatest Common Denominator for 2 numbers
+ */
 class GCDFinder {
     fun find(num1: Int, num2: Int): Int {
-        return findIteratively(num1, num2)
+        val smallestNum = Math.min(num1, num2)
+        val greatestNum = Math.max(num1, num2)
+        return findRecursively(smallestNum, greatestNum)
     }
 
+    /**
+     * Euclid's algorithm
+     */
     private fun findRecursively(num1: Int, num2: Int): Int {
-        TODO()
+        if (num2 % num1 == 0) return num1
+        return findRecursively(num2%num1, num2)
     }
 
+    /**
+     * Euclid's algorithm in iterative form
+     */
     private fun findIteratively(num1: Int, num2: Int): Int {
         println("for $num1 and $num2 divisors are:")
         val smallestNum = Math.min(num1, num2)
