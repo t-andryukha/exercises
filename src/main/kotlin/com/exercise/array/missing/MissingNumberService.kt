2e50ee39652533a6f44331d23af89e4193f1c6b8
t-andryukha <t-andryukha@yandex.ru>
2018-10-26 06:44:57 +0300

    repetition times implemented

--- src/main/kotlin/com/exercise/array/missing/MissingNumberService.kt
+++ src/main/kotlin/com/exercise/array/missing/MissingNumberService.kt
@@ -2,10 +2,12 @@ package com.exercise.array.missing
 
 class MissingNumberService {
 
-    fun getMissingNumber(array: Array<Int>): Int {
+    fun getMissingNumber(array: Array<Int>, repetitionTimes: Int = 1): Int {
         var xorSum = 0
-        for (sequentialNumber in 1 .. array.size+1) {
+        for (repetition in 1..repetitionTimes) {
+            for (sequentialNumber in 1..(array.size + 1) / repetitionTimes) {
             xorSum = xorSum.xor(sequentialNumber)
+        }
         }
         for (actualNumber in array) {
             xorSum = xorSum.xor(actualNumber)
--- src/test/kotlin/com/exercise/array/missing/MissingNumberServiceTest.kt
+++ src/test/kotlin/com/exercise/array/missing/MissingNumberServiceTest.kt
@@ -11,4 +11,10 @@ class MissingNumberServiceTest {
         assertEquals(5, MissingNumberService().getMissingNumber(array))
     }
 
+    @Test
+    internal fun shouldReturnMissingNumberWhenRepeated4TimesAgainst5() {
+        val array = arrayOf(2,7,4,6,7,12,8,1,11,9,1,3,5,2,7,4,6,12,10,8,1,11,9,10,2,7,4,6,12,3,11,9,10,3,5,2,4,5,6,12,8,1,11,9,10,3,5,2,7,4,6,12,8,1,11,9,10,3,8)
+        assertEquals(5, MissingNumberService().getMissingNumber(array, 5))
+    }
+
 }
